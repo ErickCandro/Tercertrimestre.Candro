4acb5077bc2f1822f6c227ec946310dc94e38390
erickcandro <ericksantiagocandrovalencia@gmail.com>
2023-08-01 21:35:30 -0500

    Tarea del dia lunes 31 de julio

--- excepciones.py/ContadorPrimer.py
+++ excepciones.py/ContadorPrimer.py
@@ -1,10 +1,9 @@
 from os import strerror
 
 try:
-
     counter=0
-    with open('','r') as stream:
-        char = stream.read(1)
+    stream= open("","rt")
+    char = stream.read(1)
     while char != '':
         print(char,end='')
         counter +=1
@@ -12,4 +11,4 @@ try:
     stream.close()
     print("\n\nCaracteres que contiene el archivo",counter)
 except IOError as e:
-    print("Se produjo un error de tipo E/S ",strerror(e.errno))
+    print("Se produjo un error de tipo E/S: ",strerror(e.errno))
